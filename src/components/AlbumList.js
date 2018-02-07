3a44a7e17efb9acc3b5e0cab975c763c6fa83ae7
tony.nikolov@dision.co <tony.nikolov@dision.co>
2018-02-07 09:50:48 +0200

    some changes

---
+++ src/components/AlbumList.js
@@ -0,0 +1,13 @@
+// import libraries
+import React from 'react';
+import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+// make component
+const AlbumList = () => (
+  <View>
+    <Text>Album List!!!</Text>
+  </View>
+);
+
+export default AlbumList;
